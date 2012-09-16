4e740c4cacdbe751cc677e39cee15fe708d06aad
peter-fogg <peter.fogg@oberlin.edu>
2012-09-16 14:01:28 -0400

    added first scripts

---
+++ Assets/Block.js
@@ -0,0 +1,14 @@
+#pragma strict
+
+var timestep : float;
+var health : float;
+var isMoving : boolean;
+var isOccupied : boolean;
+
+function Start () {
+    
+}
+
+function Update () {
+    
+}
---
+++ Assets/GameManager.js
@@ -0,0 +1,11 @@
+#pragma strict
+
+var board : Array;
+
+function Start () {
+    timestep = 1.0;
+}
+
+function Update () {
+    
+}
