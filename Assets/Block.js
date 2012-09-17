2ce472682ce1a2b6acb0d3f871b3411d0dcccd7d
peter-fogg <peter.fogg@oberlin.edu>
2012-09-16 23:03:17 -0400

    trying to figure out arrays; failing hard

--- Assets/Block.js
+++ Assets/Block.js
@@ -1,14 +1,21 @@
 #pragma strict
 
-var timestep : float;
 var health : float;
-var isMoving : boolean;
-var isOccupied : boolean;
+var isMoving : boolean; // solidified or not
+var tower : Tower; // might not be necessary?
 
+// Create the cube and any other bits.
 function Start () {
-    
+    // var c : GameObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
+    // c.transform.position = Vector3(0, 0, 0);
+    // gameObject.AddComponent(c);
 }
 
 function Update () {
-    
+    // if(Time.time > lastMoved + timestep) {
+    // 	transform.position = transform.position + Time.deltaTime * Vector3(0, 1, 0);
+    // }
+    // else {
+	
+    // }
 }
--- Assets/GameManager.js
+++ Assets/GameManager.js
@@ -1,14 +1,33 @@
 #pragma strict
 
-public static var board : float[,];
+public static var board : Block[,];
 public static var height : float;
 public static var width : float;
 public static var mainCamera : Camera;
 
+var timestep : float; // how often to move
+var lastMoved : float; // when we last moved
+
+public var block : Block; // for Instantiate()
+
 function Start () {
-    
+    var b : GameObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
+    b.AddComponent(Block);
+    b.transform.position = Vector3(0, 0, 0);
+//    board = 
+    board[2][1] = b;
+    timestep = 1.0;
+    lastMoved = Time.time;
 }
 
 function Update () {
-    
+    if(Time.time > lastMoved + timestep) {
+	for(var row : Block[] in board) {
+	    for(var block : Block in row) {
+		if(block != null) {
+		    block.transform.position.y += 1;
+		}
+	    }
+	}
+    }
 }
