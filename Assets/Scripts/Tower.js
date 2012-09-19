014e51acc0fc3ca302c2bac6711a9a20e5fa937d
U-Oren-PC\Oren <Oren@Oren-PC.(none)>
2012-09-19 10:12:52 -0400

    Fixed some but not all compiler errors in Tower.js. Need to read up on lists in javascript before tackling the rest.

--- Assets/Scripts/Tower.js
+++ Assets/Scripts/Tower.js
@@ -1,13 +1,13 @@
 #pragma strict
 //SCRIPT THAT HANDLES TOWER BEHAVIOR
 
-var health : float;			:float; //keeps track of how much health the tower has
-var damage : float;			:float;	//keeps track of how much damage the tower deals
+var health				:float; //keeps track of how much health the tower has
+var damage				:float;	//keeps track of how much damage the tower deals
 //var prefabBullet	:Transform; //stores the bullet that the tower fires
 var range			:float;	//keeps track of the the range of the turret
 var bfire			:float;
 //var cenemy  // best creature enemy
-var benemy : Vector3   // best block
+var benemy : Vector3;   // best block
 var base: 			Block; //keeps track of the block that the tower is placed on
 
 function Start () {
@@ -43,9 +43,11 @@ function Update () {
 }*/
 
 function battack() { // looks for a block to attack
+	var manager: GameObject = GameObject.Find("GameManager");
+	var blockList: List.<GameObject> = manager.GetComponent(GameManager).blockList;
 	// check for blocks in range
-	var min : 10;
-	for(var i : 0; i < blockList.length; i++) {
+	var min : float = 10;
+	for(var i = 0; i < blockList.length; i++) {
 		if(Vector3.Distance(blockList.get(i).position, transform.position))
 			if(blockList.get(i).isOccupied == false)
 				return blockList.get(i).position; 
@@ -53,14 +55,12 @@ function battack() { // looks for a block to attack
 	return transform.position;
 }
 
-function attack(var target){ // attack a given target
+function attack(target: GameObject){ // attack a given target
 
 	// instantiate a bullet heading in the direction of given target
 	var bullet : GameObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
 	bullet.transform.position = Vector3.Lerp(transform.position, target, Time.time);
 	Destroy(bullet);	
-}
-
 }
 
 function OnMouseDown() {
