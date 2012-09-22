5a54899d502c7c5a733f65bd73acbee88dda7fb6
oshoham <oshoham@oberlin.edu>
2012-09-22 13:37:06 -0400

    Whoops, didn't push Tower.js...

--- Assets/Scripts/Tower.js
+++ Assets/Scripts/Tower.js
@@ -2,6 +2,7 @@
 //SCRIPT THAT HANDLES TOWER BEHAVIOR
 
 var health : float; //keeps track of how much health the tower has
+var maxHealth: float;
 //var damage : float;	//keeps track of how much damage the tower deals
 var range : float;	//keeps track of the the range of the turret
 //var cenemy  // best creature enemy
@@ -9,9 +10,10 @@ var benemy : Block;   // best block
 var base : Block; //keeps track of the block that the tower is placed on
 var lastFired : float;
 var fireRate : float;
+var lerpedColor: Color = Color.white;
 
 function Start () {
-    health = 10;
+    health = maxHealth = 10;
 //    damage = 1;
     range = 10; // decide what range of tower is
     fireRate = .2; // decide what firing freq is
@@ -19,6 +21,10 @@ function Start () {
 }
 
 function Update () {
+	//Damage/color change handling:
+	lerpedColor = Color.Lerp(Color.blue, Color.white,health/maxHealth);
+   	renderer.material.color = lerpedColor;
+	
 	//if the tower's health is depleted, destroy the tower
     // also if our block is gone
 	if (health <= 0 || base == null) {
